f48efe79ac097d5eedac4e3de58f259179cc3daf
neilburt <neil.burt@comcast.net>
2021-06-13 11:56:18 -0500

    Finished .js with refined comments and spacing, changed stylesheet to a dark theme.

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,35 @@
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
+
+// Utilizing escape to allow backslash in symbol string
 var symbol = "`~!@#$%^()-_+{[}]\\:',./?";
 
 function generatePassword() {
   var lengthSelect = prompt("How long would you like your password to be? (Between 8 and 128 characters.)", 8);
-  // If user clicks "Cancel", reset
+  
+// If user clicks "Cancel", reset
   if (lengthSelect === null) {
-    return "Please choose your password length.\nClick the button again.";
+    return "Please choose your password length.\nClick the 'Generate Password' button again.";
   }
-  // If user enters incorrect value, reset
+// If user enters incorrect value, reset
   while (lengthSelect <=7 || lengthSelect >=129) {
-    return "Your password must contain between 8 and 128 characters.\nPlease click the button again.";
+    return "Your password must contain between 8 and 128 characters.\nPlease click the 'Generate Password' button again.";
   }
-  // User character-type prompts
+// User character-type prompts
   var lowerSelect = confirm("Would you like your password to include lowercase letters");
   var upperSelect = confirm("How about uppercase letters?");
   var numberSelect = confirm("Numbers?");
   var symbolSelect = confirm("Would you like symbols in there?");
-  // If user clicks "Cancel" on all character-type options, reset
+  
+// If user clicks "Cancel" on all character-type options, reset
   while (lowerSelect === false && upperSelect === false && numberSelect === false && symbolSelect === false) {
-    return "You must select at least one type of character to include in your password.\nPlease click the button again.";
+    return "You must select at least one type of character to include.\nPlease click the 'Generate Password' button again.";
   }
 
   var characterSet = "";
-  // Building character set
+  
+// Building character set
   if (lowerSelect) {
     characterSet += lower
   }
@@ -38,17 +43,17 @@ function generatePassword() {
   if (symbolSelect) {
     characterSet += symbol
   }
-  // Variable to contain final product
+// Variable to contain final product
   var randomization = ""
-  // Building final product
+  
+// Building final product
     for (var i = 0; i < lengthSelect; i++) {
     randomization = randomization + characterSet[Math.floor(Math.random() * characterSet.length)];
     }
-
     return randomization;
 }
-
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
